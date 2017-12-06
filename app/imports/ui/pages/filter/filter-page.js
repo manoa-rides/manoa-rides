7c9960634164bcfcd8a0e93d4d8fd5007150e39d
Aditya Kumar <adityak@hawaii.edu>
2017-12-05 22:58:13 -1000

    Added the value for the time, not working yet

--- app/imports/ui/pages/filter/filter-page.js
+++ app/imports/ui/pages/filter/filter-page.js
@@ -6,7 +6,7 @@ import { Interests } from '/imports/api/interest/InterestCollection';
 
 const selectedDriver = 'seletedDriverOrRider';
 const selectedSeats = 'selectedSeats';
-const selectedDriverZipcode= 'selectedDriverZipcode';
+const selectedDriverZipcode = 'selectedDriverZipcode';
 const selectedRiderZipcode = 'selectedRiderZipcode';
 
 
@@ -62,6 +62,32 @@ Template.Filter_Page.helpers({
     return [{ label: '1', selected: false }, { label: '2', selected: false }, { label: '3', selected: false },
       { label: '4', selected: false }, { label: '5', selected: false }, { label: '6', selected: false },
       { label: '7', selected: false }, { label: '8+', selected: false }];
+  },
+  timeOptions() {
+    return [{ label: ' 12:00 AM', value: '0', selected: false },
+      { label: '1:00 AM', value: '1', selected: false },
+      { label: '2:00 AM', value: '2', selected: false },
+      { label: '3:00 AM', value: '3', selected: false },
+      { label: '4:00 AM', value: '4', selected: false },
+      { label: '5:00 AM', value: '5', selected: false },
+      { label: '6:00 AM', value: '6', selected: false },
+      { label: '7:00 AM', value: '7', selected: false },
+      { label: '8:00 AM', value: '8', selected: false },
+      { label: '9:00 AM', value: '9', selected: false },
+      { label: '10:00 AM', value: '10', selected: false },
+      { label: '11:00 AM', value: '11', selected: false },
+      { label: '12:00 PM', value: '12', selected: false },
+      { label: '1:00 PM', value: '13', selected: false },
+      { label: '2:00 PM', value: '14', selected: false },
+      { label: '3:00 PM', value: '15', selected: false },
+      { label: '4:00 PM', value: '16', selected: false },
+      { label: '5:00 PM', value: '17', selected: false },
+      { label: '6:00 PM', value: '18', selected: false },
+      { label: '7:00 PM', value: '19', selected: false },
+      { label: '8:00 PM', value: '20', selected: false },
+      { label: '9:00 PM', value: '21', selected: false },
+      { label: '10:00 PM', value: '22', selected: false },
+      { label: '11:00 PM', value: '23', selected: false }];
   },
 });
 
