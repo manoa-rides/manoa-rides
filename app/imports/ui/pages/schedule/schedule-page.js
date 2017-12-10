aae4daee49411711ddb5546652e33898ff561c11
seanytak <seanytak@hawaii.edu>
2017-12-09 14:28:02 -1000

    Added Different Day Schedule Options

--- app/imports/ui/pages/schedule/schedule-page.js
+++ app/imports/ui/pages/schedule/schedule-page.js
@@ -28,32 +28,35 @@ Template.Schedule_Page.helpers({
   profile() {
     return Profiles.findDoc(FlowRouter.getParam('username'));
   },
-  times() {
+  hours() {
     const profile = Profiles.findDoc(FlowRouter.getParam('username'));
-    return profile && [{ label: ' 12:00 AM', value: '0', selected: false },
-      { label: '1:00 AM', value: '1', selected: false },
-      { label: '2:00 AM', value: '2', selected: false },
-      { label: '3:00 AM', value: '3', selected: false },
-      { label: '4:00 AM', value: '4', selected: false },
-      { label: '5:00 AM', value: '5', selected: false },
-      { label: '6:00 AM', value: '6', selected: false },
-      { label: '7:00 AM', value: '7', selected: false },
-      { label: '8:00 AM', value: '8', selected: false },
-      { label: '9:00 AM', value: '9', selected: false },
-      { label: '10:00 AM', value: '10', selected: false },
-      { label: '11:00 AM', value: '11', selected: false },
-      { label: '12:00 PM', value: '12', selected: false },
-      { label: '1:00 PM', value: '13', selected: false },
-      { label: '2:00 PM', value: '14', selected: false },
-      { label: '3:00 PM', value: '15', selected: false },
-      { label: '4:00 PM', value: '16', selected: false },
-      { label: '5:00 PM', value: '17', selected: false },
-      { label: '6:00 PM', value: '18', selected: false },
-      { label: '7:00 PM', value: '19', selected: false },
-      { label: '8:00 PM', value: '20', selected: false },
-      { label: '9:00 PM', value: '21', selected: false },
-      { label: '10:00 PM', value: '22', selected: false },
-      { label: '11:00 PM', value: '23', selected: false }];
+    return profile && [
+      { label: '12', value: '0', selected: false },
+      { label: '1', value: '1', selected: false },
+      { label: '2', value: '2', selected: false },
+      { label: '3', value: '3', selected: false },
+      { label: '4', value: '4', selected: false },
+      { label: '5', value: '5', selected: false },
+      { label: '6', value: '6', selected: false },
+      { label: '7', value: '7', selected: false },
+      { label: '8', value: '8', selected: false },
+      { label: '9', value: '9', selected: false },
+      { label: '10', value: '10', selected: false },
+      { label: '11', value: '11', selected: false }];
+  },
+  minutes() {
+    const profile = Profiles.findDoc(FlowRouter.getParam('username'));
+    return profile && [
+      { label: '00', value: '0', selected: false },
+      { label: '15', value: '1', selected: false },
+      { label: '30', value: '2', selected: false },
+      { label: '45', value: '3', selected: false }];
+  },
+  time() {
+    const profile = Profiles.findDoc(FlowRouter.getParam('username'));
+    return profile && [
+      { label: 'AM', value: '0', selected: false },
+      { label: 'PM', value: '1', selected: false }];
   },
 });
 
