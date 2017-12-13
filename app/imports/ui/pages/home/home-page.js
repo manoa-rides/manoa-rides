938783e95c0bf2c7d7033ce2b9cb1da24212ea84
seanytak <seanytak@hawaii.edu>
2017-12-12 23:13:11 -1000

    Added schedule template into the Home Page

--- app/imports/ui/pages/home/home-page.js
+++ app/imports/ui/pages/home/home-page.js
@@ -28,6 +28,14 @@ Template.Home_Page.helpers({
   profile() {
     return Profiles.findDoc(FlowRouter.getParam('username'));
   },
+  times() {
+    return ['12am', '1am', '2am', '3am', '4am', '5am', '6am', '7am', '8am', '9am', '10am', '11am',
+      '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm', '9pm', '10pm', '11pm'];
+  },
+  colorCell(day, time) {
+    const rideTimes = Profiles.findDoc(FlowRouter.getParam('username')).rideTimes;
+    return time === ;
+  }
 });
 
 
